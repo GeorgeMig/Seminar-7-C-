e62a7f63c14dfd247a256d706899048e1a29bd67
George_M <msgoha555@yandex.ru>
2022-06-05 21:51:24 +0300

    Complete task 48

---
+++ Task_48/Program.cs
@@ -0,0 +1,29 @@
+﻿//ПРОГРАММА, КОТОРАЯ ПОЗВОЛЯЕТ НАХОДИТЬ ЭЛЕМЕНТЫ ДВУМЕРНОГО МАССИВА ПО ФОРМУЛЕ m+n
+
+Console.Write("Введите m = ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите n = ");
+int n = int.Parse(Console.ReadLine());
+int[,] array = new int [m,n];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = i + j;
+
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+
+        }
+        Console.WriteLine();
+    }   
+}
+PrintArray(array);
