e5f117fe7728faa525363cc072e7ba82c9d4fbf5
George_M <msgoha555@yandex.ru>
2022-06-05 22:27:10 +0300

    Comleted task_49

---
+++ Task_49/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write("Введите m = ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите n = ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] GetArray(int a, int b)
+{
+    int[,] array = new int[a, b];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(1, 6);
+
+        }
+    }
+    return array;
+}
+
+int[,] arrRes = GetArray(m, n); //метод создания массива 
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+
+        }
+        Console.WriteLine();
+    }
+}
+PrintArray(arrRes);// печать массива
+
+for (int i = 1; i < arrRes.GetLength(0); i++)
+{
+    for (int j = 1; j < arrRes.GetLength(1); j++)
+    {
+        if (i % 2 == 0 && j % 2 == 0)
+        {
+            arrRes[i, j] = arrRes[i, j] * arrRes[i, j];
+        }
+
+    }
+}
+
+Console.WriteLine();
+
+PrintArray(arrRes);
